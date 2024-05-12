83a3fcbcb41c1ed05c3e9fbef71395bd8f3587f0
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2024-05-12 13:53:20 +0700

    Component Inti dari React Native (2)

--- ProgateReactNative/App.js
+++ ProgateReactNative/App.js
@@ -1,11 +1,108 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Button, FlatList, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+// Flatlist
+// const items = [
+//   {
+//     id: "1",
+//     title: "Item Pertama",
+//   },
+//   {
+//     id: "2",
+//     title: "Item Kedua",
+//   },
+//   {
+//     id: "3",
+//     title: "Item Ketiga",
+//   },
+//   {
+//     id: "4",
+//     title: "Item Keempat",
+//   },
+//   {
+//     id: "5",
+//     title: "Item Kelima",
+//   },
+//   {
+//     id: "6",
+//     title: "Item Keenam",
+//   },
+//   {
+//     id: "7",
+//     title: "Item Ketujuh",
+//   },
+//   {
+//     id: "8",
+//     title: "Item Kedelapan",
+//   },
+//   {
+//     id: "9",
+//     title: "Item Kesembilan",
+//   },
+//   {
+//     id: "10",
+//     title: "Item Kesepuluh",
+//   },
+// ];
+
+// const Item = ({ title }) => (
+//   <View style={styles.item}>
+//     <Text style={styles.title}>{title}</Text>
+//   </View>
+// );
+
+// ScrollView
+// const items = [
+//   {
+//     id: "1",
+//     title: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+//   },
+//   {
+//     id: "2",
+//     title: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+//   },
+//   {
+//     id: "3",
+//     title: "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+//   },
+//   {
+//     id: "4",
+//     title: "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+//   },
+//   {
+//     id: "5",
+//     title: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+//   },
+//   {
+//     id: "6",
+//     title: "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+//   },
+// ];
+
+// Kasus penggunakan ScrollView diganti dengan flatlist
+// const items = [
+//   {
+//     id: "1",
+//     title: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+//   },
+// ];
+
+// const Item = ({ title }) => (
+//   <View style={styles.item}>
+//     <Text style={styles.title}>{title}</Text>
+//   </View>
+// );
 
 export default function App() {
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
-  const [question, setQuestion] = useState();
+  // const [name, setName] = useState();
+  // const [age, setAge] = useState();
+  // const [question, setQuestion] = useState();
+  // const renderItem = ({ item }) => <Item title={item.title} />;
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(count + 1);
+  console.log("tekan tombol===>", onPress);
+
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="black" style="light" />
@@ -43,11 +140,53 @@ export default function App() {
       </View> */}
 
       {/* TextInput */}
-      <View>
+      {/* <View>
         <TextInput style={styles.input} placeholder="Masukan Nama" onChangeText={setName} value={name} />
         <TextInput style={styles.input} placeholder="Masukan Umur" onChangeText={setAge} value={age} keyboardType="numeric" />
         <TextInput style={styles.multiLineInput} placeholder="Masukan pertanyaan" multiline={true} numberOfLines={3} onChangeText={setQuestion} value={question} />
+      </View> */}
+
+      {/* ScrollView */}
+      {/* <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+          aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </ScrollView> */}
+
+      {/* <ScrollView>
+        {items.map((item) => (
+          <View key={item.id} style={styles.item}>
+            <Text style={styles.title}>{item.title}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+
+      {/* FlatList */}
+      {/* <FlatList data={items} renderItem={renderItem} keyExtractor={(item) => item.id} /> */}
+
+      {/* Button */}
+      <View style={styles.buttonWrapper}>
+        <Button title="Default Button" onPress={() => Alert.alert("Simple Button")} />
       </View>
+      <View style={styles.buttonWrapper}>
+        <Button title="Color Button" color="#f194ff" onPress={() => Alert.alert("Color Button")} />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title="Disabled Button" disabled onPress={() => Alert.alert("Disable Button")} />
+      </View>
+
+      {/* TouchableOpacity */}
+      <Text>You Clicked {count} times</Text>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>Press Here</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -62,7 +201,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    backgroundColor: "#eaeaea",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
   // title: {
   //   marginTop: 16,
@@ -80,16 +221,43 @@ const styles = StyleSheet.create({
   //   width: 200,
   //   height: 200,
   // },
-  input: {
-    height: 60,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  // input: {
+  //   height: 60,
+  //   margin: 12,
+  //   borderWidth: 1,
+  //   padding: 10,
+  // },
+  // multiLineInput: {
+  //   height: 100,
+  //   margin: 12,
+  //   borderWidth: 1,
+  //   padding: 10,
+  // },
+  // scrollView: {
+  //   backgroundColor: "gray",
+  //   marginHorizontal: 20,
+  // },
+  // text: {
+  //   fontSize: 42,
+  // },
+  // item: {
+  //   backgroundColor: "#f9c2ff",
+  //   padding: 20,
+  //   marginVertical: 8,
+  //   marginHorizontal: 16,
+  // },
+  // title: {
+  //   fontSize: 32,
+  // },
+  buttonWrapper: {
+    margin: 10,
   },
-  multiLineInput: {
-    height: 100,
-    margin: 12,
-    borderWidth: 1,
+  button: {
+    backgroundColor: "#674f74",
     padding: 10,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "#FFF",
   },
 });
