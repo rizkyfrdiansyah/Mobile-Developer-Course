308c2e097945326995a6507a93a4ee1fca847630
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2024-05-14 15:29:31 +0700

    Menggunakan API dalam React Native App

--- ProgateWeatherApp/App.js
+++ ProgateWeatherApp/App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function App() {
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setData(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item.id} />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  item: {
+    backgroundColor: "grey",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    color: "white",
+    fontSize: 32,
   },
 });
