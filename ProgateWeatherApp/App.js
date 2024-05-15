1292a93645c4474f8e368455e8d85d610ee86aa4
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2024-05-15 21:46:10 +0700

    Proyek Weather App Selesai

--- ProgateWeatherApp/App.js
+++ ProgateWeatherApp/App.js
@@ -1,49 +1,95 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from "react-native";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import WeatherSearch from "./src/components/WeatherSearch";
+import WeatherInfo from "./src/components/WeatherInfo";
+import { API_KEY, BASE_URL } from "./src/components/Constant";
 
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
+// const Item = ({ title }) => (
+//   <View style={styles.item}>
+//     <Text style={styles.title}>{title}</Text>
+//   </View>
+// );
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const renderItem = ({ item }) => <Item title={item.title} />;
+  const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState("");
 
-  useEffect(() => {
+  const renderComponent = () => {
+    switch (status) {
+      case "loading":
+        return <ActivityIndicator size="large" />;
+      case "success":
+        return <WeatherInfo weatherData={weatherData} />;
+      case "error":
+        return <Text>Something went wrong. Please try again with a correct city name.</Text>;
+      default:
+        return;
+    }
+  };
+
+  const searchWeather = (location) => {
+    setStatus("loading");
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then((response) => {
-        setData(response.data);
-        console.log(response);
+        const data = response.data;
+        data.visibility /= 1000;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15;
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+        setStatus("success");
       })
       .catch((error) => {
-        console.log(error);
+        setStatus("error");
       });
-  });
+  };
+
   return (
     <View style={styles.container}>
-      <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item.id} />
+      <WeatherSearch searchWeather={searchWeather} />
+      <View style={styles.marginTop20}>{renderComponent()}</View>
     </View>
   );
+  // const [data, setData] = useState([]);
+  // const renderItem = ({ item }) => <Item title={item.title} />;
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts")
+  //     .then((response) => {
+  //       setData(response.data);
+  //       console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
+  // return (
+  //   <View style={styles.container}>
+  //     <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item.id} />
+  //   </View>
+  // );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  item: {
-    backgroundColor: "grey",
     padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
   },
-  title: {
-    color: "white",
-    fontSize: 32,
+  marginTop20: {
+    marginTop: 20,
   },
+  // item: {
+  //   backgroundColor: "grey",
+  //   padding: 20,
+  //   marginVertical: 8,
+  //   marginHorizontal: 16,
+  // },
+  // title: {
+  //   color: "white",
+  //   fontSize: 32,
+  // },
 });
---
+++ ProgateWeatherApp/src/components/Constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+export const API_KEY = "4318947d051eec6884335a13a0d005dd";
