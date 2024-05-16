bdf2c693f7560ba68d5b5acb75759b6de72f92a3
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2024-05-16 15:56:07 +0700

    Pengenalan React Navigation

--- ProgateNavigation/App.js
+++ ProgateNavigation/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Pergi ke Progate"
+        onPress={() =>
+          navigation.navigate("Progate", {
+            name: "Ninja Ken",
+            language: "React Native",
+          })
+        }
+      />
+    </View>
+  );
+};
+
+const ProgateScreen = ({ navigation, route }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Progate {route.params.language}!</Text>
+      <Text>Ayo belajar {route.params.language}!</Text>
+      <Button title="Kembali" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Progate" component={ProgateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
