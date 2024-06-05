3283442827a6b61516b73142facbe1e56f8f0ed7
rizkyfrdiansyah <muhammadrizkyferdiansyah@gmail.com>
2024-06-05 15:16:58 +0700

    JavaScript 7 Selesai

---
+++ JavaScriptDasar/JavaScript7/CallbackFunction/DeklarasiCallbackFunction/script.js
@@ -0,0 +1,16 @@
+const printKen = () => {
+    console.log("Ninja Ken");
+  };
+  
+  const call = (callback) => {
+    console.log("Memanggil callback function.");
+    callback();
+  };
+  
+  call(printKen);
+  
+  // Deklarasikan function didalam argument dan teruskan
+  call(() => {
+    console.log("Guru Domba");
+  });
+  
